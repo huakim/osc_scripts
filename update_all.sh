5bcc1948c2712c3262be612b78a1004d78b3e236
huakim <92579808+huakim@users.noreply.github.com>
2024-10-11 14:25:16 +0300

    Add files via upload

---
+++ update_all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -n "${NOUPDATE}" ]]
+then
+updatejob(){
+}
+else
+updatejob(){
+ echo fine
+}
+fi
+
+job(){
+for i in "${@}"
+do
+ if [[ -d "$i" ]]
+ then
+  (
+  cd "$i"
+  updatejob
+  osc service mr
+  osc add *
+  osc ci -m update
+  osc service rr
+  osc rebuild
+  )
+ fi
+done
+}
+
+if (( "${#}" ))
+then
+  job "${@}"
+else
+  job *
+fi
